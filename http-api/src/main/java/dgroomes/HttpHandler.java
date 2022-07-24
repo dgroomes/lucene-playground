bfda80e6ed19e7fe9b71805a1b86c1adad1af9ce
David Groomes <dwgroomes@gmail.com>
2022-07-24 16:16:20 -0500

    Encapsulate search stuff in TimeZoneSearchSystem.java

--- http-api/src/main/java/dgroomes/HttpHandler.java
+++ http-api/src/main/java/dgroomes/HttpHandler.java
@@ -7,7 +7,6 @@ import org.apache.hc.core5.http.io.HttpRequestHandler;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.http.protocol.HttpContext;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.store.Directory;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,16 +16,16 @@ import java.util.List;
  */
 class HttpHandler implements HttpRequestHandler {
 
-  private final Directory indexDir;
+  private final TimeZoneSearchSystem timeZoneSearchSystem;
 
-  public HttpHandler(Directory indexDir) {
-    this.indexDir = indexDir;
+  public HttpHandler(TimeZoneSearchSystem timeZoneSearchSystem) {
+    this.timeZoneSearchSystem = timeZoneSearchSystem;
   }
 
   @Override
   public void handle(final ClassicHttpRequest request, final ClassicHttpResponse response, final HttpContext context) throws HttpException, IOException {
     String keyword = parseKeyword();
-    List<Document> results = Runner.search(indexDir, keyword);
+    List<Document> results = timeZoneSearchSystem.search(keyword);
     var msg = "Found %d results for the search.";
     var responseBody = new StringEntity(msg.formatted(results.size()));
     response.setEntity(responseBody);
--- http-api/src/main/java/dgroomes/Runner.java
+++ http-api/src/main/java/dgroomes/Runner.java
@@ -6,26 +6,12 @@ import org.apache.hc.core5.io.CloseMode;
 import org.apache.hc.core5.util.TimeValue;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
-import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
 
 /**
  * A demonstration that exposes Lucene search as an HTTP API. See the README for more information.
@@ -40,8 +26,8 @@ public class Runner {
     try (Directory indexDir = new ByteBuffersDirectory();
          Analyzer analyzer = new StandardAnalyzer()) {
 
-      indexData(indexDir, analyzer);
-      runServerContinuously(indexDir);
+      TimeZoneSearchSystem searchSystem = TimeZoneSearchSystem.init(indexDir, analyzer);
+      runServerContinuously(searchSystem);
     } catch (IOException e) {
       log.error("Unexpected error", e);
       throw new RuntimeException(e);
@@ -49,81 +35,10 @@ public class Runner {
   }
 
   /**
-   * Search for the given keyword.
-   */
-  public static List<Document> search(Directory indexDir, String keyword) {
-    DirectoryReader reader = null;
-    try {
-      reader = DirectoryReader.open(indexDir);
-    } catch (IOException e) {
-      throw new IllegalStateException("Unexpected error opening the Lucene index", e);
-    }
-
-    log.info("Searching for time zones using the keyword: '{}'", keyword);
-    IndexSearcher searcher = new IndexSearcher(reader);
-    StandardQueryParser queryParser = new StandardQueryParser(new StandardAnalyzer());
-
-    List<ScoreDoc> hits;
-
-    try {
-      Query query = queryParser.parse(keyword, Indexer.FIELD_TIME_ZONE_DISPLAY_NAME);
-      TopDocs results = searcher.search(query, 2000);
-      ScoreDoc[] packageNameHits = results.scoreDocs;
-      hits = List.of(packageNameHits);
-    } catch (QueryNodeException | IOException e) {
-      throw new IllegalStateException("Unexpected error while searching", e);
-    }
-
-    log.info("Found {} hits", hits.size());
-
-    return hits.stream()
-            .map(hit -> {
-              try {
-                return searcher.doc(hit.doc);
-              } catch (IOException e) {
-                throw new IllegalStateException("Unexpected error while getting the document from the index", e);
-              }
-            })
-            .toList();
-  }
-
-  /**
-   * Index the domain data into an in-memory Lucene index.
-   */
-  private static void indexData(Directory indexDir, Analyzer analyzer) {
-    try (var indexWriter = indexWriter(indexDir, analyzer)) {
-
-      List<String> timeZoneDisplayNames = findTimeZones();
-      log.info("Indexing {} known time zones.", timeZoneDisplayNames.size());
-
-      var indexer = new Indexer(indexWriter);
-      for (var timeZoneDisplayName : timeZoneDisplayNames) {
-        indexer.index(timeZoneDisplayName);
-      }
-
-      log.info("Indexing done.");
-    } catch (Exception e) {
-      log.error("Unexpected error while indexing.", e);
-      System.exit(1);
-    }
-  }
-
-  private static List<String> findTimeZones() {
-    String[] timeZoneIds = TimeZone.getAvailableIDs();
-    return Arrays.stream(timeZoneIds)
-            .map(id -> TimeZone.getTimeZone(id).getDisplayName(Locale.US))
-            // Filter out "GMT" because the results are not interesting. They just describe offsets not places.
-            .filter(displayName -> !displayName.startsWith("GMT"))
-            .distinct()
-            .sorted()
-            .toList();
-  }
-
-  /**
    * Run the HTTP server. This runs continuously until the process is stopped with "Ctrl + C".
    */
-  private static void runServerContinuously(Directory indexDir) throws IOException {
-    var simulatorHttpHandler = new HttpHandler(indexDir);
+  private static void runServerContinuously(TimeZoneSearchSystem timeZoneSearchSystem) throws IOException {
+    var simulatorHttpHandler = new HttpHandler(timeZoneSearchSystem);
 
     ServerBootstrap builder = ServerBootstrap.bootstrap()
             .setListenerPort(PORT)
@@ -137,14 +52,5 @@ public class Runner {
     } catch (InterruptedException e) {
       log.error("The server was interrupted.", e);
     }
-  }
-
-  private static IndexWriter indexWriter(Directory dir, Analyzer analyzer) throws IOException {
-    IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-    // This configuration removes any pre-existing index files.
-    config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-
-    return new IndexWriter(dir, config);
   }
 }
